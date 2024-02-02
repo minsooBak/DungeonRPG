da020039380e9a68f6d53b19ad413bbfb053bebe
minsooBak <minsoo9716@gmail.com>
2024-02-02 21:11:30 +0900

    feat : Inventory Script
    UI테스트용 코드

--- Assets/Scripts/Entities/UI/Inventory.cs
+++ Assets/Scripts/Entities/UI/Inventory.cs
@@ -1,6 +1,15 @@
-using UnityEngine;
+using TMPro;
 
 public class Inventory : UIBase
 {
-
+    public TextMeshProUGUI text1;
+    public TextMeshProUGUI text2;
+    private void Awake()
+    {
+        if (prefab != null)
+            return;
+        text1.text = "NAME";
+        text2.text = "DIE";
+        //Disable();
+    }
 }
