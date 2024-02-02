33d250bdadfe96577de7a43039b0b78c6fe0b055
minsooBak <minsoo9716@gmail.com>
2024-02-02 21:09:51 +0900

    feat : UIManager Script

--- Assets/ScriptableObjects/Scripts/Manager/UIManager.cs
+++ Assets/ScriptableObjects/Scripts/Manager/UIManager.cs
@@ -4,36 +4,50 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="UIManager", menuName = "Data/UIManager")]
 public class UIManager : ScriptableObject
 {
-    public List<UIBase> uIBases = new();
+    public List<UIBase> uiBases = new();
     public GameObject uiBasePrefab;
-    private GameObject obj;
+    private GameObject _obj;
+
     public T GetUI<T>() where T : UIBase
     {
-        foreach(UIBase u in uIBases)
+        foreach(UIBase ui in uiBases)
         {
-
-        }
-        if (obj == null)
-            obj = Instantiate(uiBasePrefab);
-
-        T data = obj.GetComponentInChildren<T>();
-        if(obj.TryGetComponent<T>(out data))
-        {
-            return data;
+            if(ui is T)
+            {
+                return ui as T;
+            }
         }
 
-        data = obj.AddComponent<T>();
-        return data;
+        if (_obj == null)
+        {
+            CreateBasePrefab();
+        }
+
+        T data = _obj.GetComponent<T>();
+        T result = CreateUIPrefab(data.prefab, _obj.transform).GetComponent<T>();
+        Destroy(data);
+        uiBases.Add(result);
+        return result;
     }
 
     public void RemoveUI<T>(T data) where T : UIBase
     {
-        Destroy(obj.GetComponent<T>());
-        uIBases.Remove(data);
+        Destroy(_obj.GetComponent<T>());
+        uiBases.Remove(data);
     }
 
     public void Clear()
     {
-        uIBases.Clear();
+        uiBases.Clear();
+    }
+
+    private void CreateBasePrefab()
+    {
+        _obj = Instantiate(uiBasePrefab);
+    }
+
+    private GameObject CreateUIPrefab(GameObject obj, Transform transform)
+    {
+        return Instantiate(obj, transform);
     }
 }
